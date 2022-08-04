531fa7d544695f8bc1835b4db8385dab9ffee4bd
Sergei052 <sergei2266249@gmail.com>
2022-08-04 18:18:02 +0300

    1

--- parsing_test.py
+++ parsing_test.py
@@ -3,17 +3,22 @@ from bs4 import BeautifulSoup
 
 
 
-url = "https://health-diet.ru/table_calorie/?utm_source=leftMenu&utm_medium=table_calorie"
+# url = "https://health-diet.ru/table_calorie/?utm_source=leftMenu&utm_medium=table_calorie"
+#
+# headers = {
+#     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0'
+# }
+# req = requests.get(url, headers=headers)
+# src = req.text  # получаю код страницы
+# # print(src)
+#
+# with open("index.html", "w") as file:   # сохраняю код страницы в файл
+#     file.write(src)
+# так как код запроса уже не нужен, закомментирую его
 
-headers = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0'
-}
-req = requests.get(url, headers=headers)
-src = req.text  # получаю код страницы
-# print(src)
+with open("index.html") as file:
+     src = file.read()
 
-with open("index.html", "w") as file:   # сохраняю код страницы в файл
-    file.write(src)
-
+soup = BeautifulSoup(src, "lxml")   # создал переменную в которую передал, в качестве параметров парсер lxml и переменную src
 
